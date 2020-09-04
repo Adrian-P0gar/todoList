439d856150ce52329ca56a868350abdab806a308
Adrian-P0gar <pogar.adi@gmail.com>
2020-09-04 16:22:39 +0300

    submit form and insert data in db

--- add.php
+++ add.php
@@ -13,65 +13,93 @@
 
     <title>Document</title>
     <?php require 'navbar.php';
+    require 'dbConnection.php';
     ?>
 </head>
 <body>
-        <h1> ruta de add</h1>
+        <h1> Add new tasks! </h1>
 
-        <div id="forms">
-            <form action="add.php" method="post" id="0">
-            <input type="text" name="name" id="name0">
-            <input type="text" name="description" id="description0">
+
+            <form action="add.php" method="post" id="form">
+                <div id="input">
+                    <p>
+                        <label for="name0"> Name: </label>
+                        <input type="text" name="name[]" id="name0" >
+                        <label for="description0"> Description: </label>
+                        <input type="text" name="description[]" id="description0">
+                        <label for="dueDate0">Due Date:</label>
+                        <input type="date" id="dueDate0" name="dueDate[]">
+                    </p>
+                </div>
+                <button type="submit" value="Submit" >Submit</button>
+                <button type="button" onclick="addNewTask()"> New Task  </button>
             </form>
 
 
-        </div>
-
-        <button type="button" onclick="addNewTask()"> New Task  </button>
-        <button type="button" onclick="submitAll()"> Submit  </button>
         <?php
-            foreach ($_POST as $postVar){
-                echo $postVar. "sd";
+        if(count($_POST) > 0){
+
+            $counter = 0;
+
+            while ($counter < count($_POST['name'])){
+                $query = "INSERT INTO tasks (name ,description, duedate) VALUES ('".$_POST['name'][$counter]."','".$_POST['description'][$counter]."','".$_POST['dueDate'][$counter]."')";
+
+                if(mysqli_query($conn , $query)){
+                    echo "good";
+                }
+                else{
+                  echo  mysqli_error($conn) ;
+                }
+                $counter++;
             }
+        }
+
 
 
         ?>
-
             <script type="text/javascript">
                 let counter = 0;
                 addNewTask = function (){
                     counter++;
-              const doc =  document.getElementById('forms');
-              const newForm = document.createElement("form");
-              newForm.setAttribute('method','post');
-              newForm.setAttribute('action',"add.php");
-                newForm.setAttribute('id',counter.toString());
-                let iName = document.createElement("input");
-                iName.setAttribute('type',"text");
-                iName.setAttribute('name','name');
-                iName.setAttribute("id", "name"+counter)
-                let iDescription = document.createElement("input");
-                iDescription.setAttribute('type',"text");
-                iDescription.setAttribute('name',"description");
-                iDescription.setAttribute("id", "description"+counter)
-                newForm.appendChild(iName);
-                newForm.appendChild(iDescription);
-                doc.appendChild(newForm);
+                    const doc =  document.getElementById('input');
+
+                    let iName = document.createElement("input");
+                    iName.setAttribute('type',"text");
+                    iName.setAttribute('name','name[]');
+                    iName.setAttribute("id", "name"+counter)
+                    let labelName = document.createElement('label');
+                    labelName.setAttribute("id", 'lbName'+ counter);
+                    labelName.setAttribute("for", 'name'+ counter);
+                    let labelDescription = document.createElement('label');
+                    labelDescription.setAttribute("id", 'lbDescription'+ counter);
+                    labelDescription.setAttribute("for", 'description'+ counter);
+                    let iDescription = document.createElement("input");
+                    iDescription.setAttribute('type',"text");
+                    iDescription.setAttribute('name',"description[]");
+                    iDescription.setAttribute("id", "description"+counter)
+                    let labelDate = document.createElement('label');
+                    labelDate.setAttribute("id", 'dueDate'+ counter);
+                    labelDate.setAttribute("for", 'dueDate'+ counter);
+                    let iDate = document.createElement("input");
+                    iDate.setAttribute('type',"date");
+                    iDate.setAttribute('name',"dueDate[]");
+                    iDate.setAttribute("id", "dueDate"+counter)
+
+                    let par = document.createElement("p");
+                    par.appendChild(labelName);
+                    par.appendChild(iName);
+                    par.appendChild(labelDescription);
+                    par.appendChild(iDescription)
+                    par.appendChild(labelDate);
+                    par.appendChild(iDate)
+                    doc.appendChild(par);
+
+                    document.getElementById('lbName'+counter).innerHTML = "Name: ";
+                    document.getElementById('lbDescription'+counter).innerHTML = " Description: ";
+                    document.getElementById('dueDate'+counter).innerHTML = "Due Date: ";
              }
-            submitAll = function (){
-                let x = []
-                let i=0;
-               while (i<=counter){
-                   // document.getElementById(i.toString()).submit();
-                   x.push(document.getElementById(i.toString()))
 
-                   i++;
-               }
-               let dataStr = JSON.stringify(x);
-               console.log(document.getElementById("name0").value);
-                console.log(document.getElementById("description0").value);
 
-            }
 
     </script>
 
